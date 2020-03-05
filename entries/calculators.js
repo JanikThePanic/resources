3ade420c2a3e22e238c9bf3a38e9e88b3512e78f
Janik <48893696+JanikThePanic@users.noreply.github.com>
2020-03-05 00:07:44 -0500

    entries and css

---
+++ entries/calculators.js
@@ -0,0 +1,4 @@
+// entries fopr calcutors
+// goes to list on index
+
+greatList.add({ name: '<a class="out" href = "https://sdp-si.com/eStore/CenterDistanceDesigner">Belts Calculator</a>', desc: '- Simple to use calculator to determine belt lengths and center to center distances', type: 'Calculators' });
