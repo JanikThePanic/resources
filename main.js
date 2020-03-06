521aba3e63fed8c64a92f360a386e122f15d4bb0
Janik <jk4abdl@gmail.com>
2020-03-06 16:04:23 +0000

    added darkmode

--- main.js
+++ main.js
@@ -1,43 +1,45 @@
 // main js
 
 
-    // sets vars that are needed for entry
-    var options = {
-      valueNames: ['name', 'desc', 'type']
-    };
+// sets vars that are needed for entry
+var options = {
+  valueNames: ['name', 'desc', 'type']
+};
 
-    var greatList = new List('repo', options);
+var greatList = new List('repo', options);
 
 
+// darkmode
+new Darkmode({ label: '🌓' }).showWidget();
 
-    // Opens outside pages in new tabe
-    $(document).ready(function() {
+// Opens outside pages in new tabe
+$(document).ready(function () {
 
-   $("a[href^=http]").each(function(){
+  $("a[href^=http]").each(function () {
 
-      // NEW - excluded domains list
-      var excludes = [
-         'excludeddomain1.com',
-         'excludeddomain2.com',
-         'excluded.subdomain.com'
-      ];
-      for(i=0; i<excludes.length; i++) {
-         if(this.href.indexOf(excludes[i]) != -1) {
-            return true; // continue each() with next link
-         }
+    // NEW - excluded domains list
+    var excludes = [
+      'excludeddomain1.com',
+      'excludeddomain2.com',
+      'excluded.subdomain.com'
+    ];
+    for (i = 0; i < excludes.length; i++) {
+      if (this.href.indexOf(excludes[i]) != -1) {
+        return true; // continue each() with next link
       }
+    }
 
-      if(this.href.indexOf(location.hostname) == -1) {
+    if (this.href.indexOf(location.hostname) == -1) {
 
-           // attach a do-nothing event handler to ensure we can 'trigger' a click on this link
-           $(this).click(function() { return true; }); 
+      // attach a do-nothing event handler to ensure we can 'trigger' a click on this link
+      $(this).click(function () { return true; });
 
-           $(this).attr({
-               target: "_blank",
-               title: "Opens in a new window"
-           });
+      $(this).attr({
+        target: "_blank",
+        title: "Opens in a new window"
+      });
 
-           $(this).click(); // trigger it
-      }
-   })
+      $(this).click(); // trigger it
+    }
+  })
 });
