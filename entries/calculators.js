14f75315dcacfaf9fdb57f463ad9b29841d3c4db
Janik <48893696+JanikThePanic@users.noreply.github.com>
2020-03-08 13:36:21 -0400

    removed out class from entries

--- entries/calculators.js
+++ entries/calculators.js
@@ -1,6 +1,6 @@
 // entries fopr calcutors
 // goes to list on index
 
-greatList.add({ name: '<a class="out" href = "https://sdp-si.com/eStore/CenterDistanceDesigner">Belts Calculator</a>', desc: 'Simple to use calculator to determine belt lengths and center to center distances.', type: 'Calculators' });
+greatList.add({ name: '<a href = "https://sdp-si.com/eStore/CenterDistanceDesigner">Belts Calculator</a>', desc: 'Simple to use calculator to determine belt lengths and center to center distances.', type: 'Calculators' });
 
-greatList.add({ name: '<a class="out" href = "https://sdp-si.com/eStore/CenterDistanceDesigner">Belts Calculator</a>', desc: 'Simple to use calculator to determine belt lengths and center to center distances. Hey Janik, have you ever considered not doing that. Would really help. Thanks man! Just letting myself know.', type: 'Calculators' });
+greatList.add({ name: '<a href = "https://sdp-si.com/eStore/CenterDistanceDesigner">Belts Calculator</a>', desc: 'Simple to use calculator to determine belt lengths and center to center distances.', type: 'Calculators' });
