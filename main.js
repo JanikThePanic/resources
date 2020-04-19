b4f214bed849b3b3a0da38e0f400bcbf441ca438
Janik <48893696+JanikThePanic@users.noreply.github.com>
2020-04-19 01:59:12 -0400

    test

--- main.js
+++ main.js
@@ -14,10 +14,36 @@ const darkmode = new darken({
 
 
 // Open external links in a new window or tab.
-var links = document.links;
-for (let i = 0, linksLength = links.length; i < linksLength; i++) {
-  if (links[i].hostname !== window.location.hostname) {
-    links[i].target = '_blank';
-    links[i].rel = 'noreferrer noopener';
+function ready(fn) {
+  if (document.readyState != 'loading') {
+    fn();
+  } else if (document.addEventListener) {
+    document.addEventListener('DOMContentLoaded', fn);
+  } else {
+    document.attachEvent('onreadystatechange', function () {
+      if (document.readyState != 'loading')
+        fn();
+    });
   }
 }
+
+ready(function () {
+
+  var website = window.location.hostname;
+
+  var internalLinkRegex = new RegExp('^((((http:\\/\\/|https:\\/\\/)(www\\.)?)?'
+    + website
+    + ')|(localhost:\\d{4})|(\\/.*))(\\/.*)?$', '');
+
+  var anchorEls = document.querySelectorAll('a');
+  var anchorElsLength = anchorEls.length;
+
+  for (var i = 0; i < anchorElsLength; i++) {
+    var anchorEl = anchorEls[i];
+    var href = anchorEl.getAttribute('href');
+
+    if (!internalLinkRegex.test(href)) {
+      anchorEl.setAttribute('target', '_blank');
+    }
+  }
+});
