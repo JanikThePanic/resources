f1a5f958496c5425283d9015e57ad6a41d0a83e4
Janik <48893696+JanikThePanic@users.noreply.github.com>
2020-04-22 00:49:14 -0400

    omg, please work

--- main.js
+++ main.js
@@ -14,6 +14,36 @@ const darkmode = new darken({
 
 
 // Open external links in a new window or tab.
-$('a').filter(function() {
-  return this.hostname && this.hostname !== location.hostname;
-}).addClass("external");
+function ready(fn) {
+  if (document.readyState != 'loading') {
+    fn();
+  } else if (document.addEventListener) {
+    document.addEventListener('DOMContentLoaded', fn);
+  } else {
+    document.attachEvent('onreadystatechange', function () {
+      if (document.readyState != 'loading')
+        fn();
+    });
+  }
+}
+
+ready(function () {
+
+  var website = window.location.hostname;
+
+  var internalLinkRegex = new RegExp('^((((http:\\/\\/|https:\\/\\/)(www\\.)?)?'
+    + website
+    + ')|(localhost:\\d{4})|(\\/.*))(\\/.*)?$', '');
+
+  var anchorEls = document.querySelectorAll('a');
+  var anchorElsLength = anchorEls.length;
+
+  for (var i = 0; i < anchorElsLength; i++) {
+    var anchorEl = anchorEls[i];
+    var href = anchorEl.getAttribute('href');
+
+    if (!internalLinkRegex.test(href)) {
+      anchorEl.setAttribute('target', '_blank');
+    }
+  }
+});
