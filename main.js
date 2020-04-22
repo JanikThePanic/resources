29674d4fa053ff211a13f0f1de8e46662fb28e71
Janik <48893696+JanikThePanic@users.noreply.github.com>
2020-04-22 00:45:30 -0400

    please work

--- main.js
+++ main.js
@@ -14,33 +14,6 @@ const darkmode = new darken({
 
 
 // Open external links in a new window or tab.
-$(document).ready(function () {
-
-  $("a[href^=http]").each(function () {
-
-    // NEW - excluded domains list
-    var excludes = [
-      'excludeddomain1.com',
-      'excludeddomain2.com',
-      'excluded.subdomain.com'
-    ];
-    for (i = 0; i < excludes.length; i++) {
-      if (this.href.indexOf(excludes[i]) != -1) {
-        return true; // continue each() with next link
-      }
-    }
-
-    if (this.href.indexOf(location.hostname) == -1) {
-
-      // attach a do-nothing event handler to ensure we can 'trigger' a click on this link
-      $(this).click(function () { return true; });
-
-      $(this).attr({
-        target: "_blank",
-        title: "Opens in a new window"
-      });
-
-      $(this).click(); // trigger it
-    }
-  })
-});
+$('a').filter(function() {
+  return this.hostname && this.hostname !== location.hostname;
+}).addClass("external");
